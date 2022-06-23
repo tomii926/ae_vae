e2aabca1d59cef2dfcb87dddce9a502609c90c2e
Eisuke15 <hideyu41691@gmail.com>
2022-06-23 18:39:19 +0900

    anomaly detection

---
+++ anomaly_detection.py
@@ -0,0 +1,103 @@
+import os
+from argparse import ArgumentParser
+from tokenize import Single
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+
+from common import device, mkdir_if_not_exists, net_path
+from dataset import SingleMNIST, transform
+from net import AE, VAE
+import numpy as np
+from tqdm import tqdm
+
+device = device()
+
+parser = ArgumentParser()
+parser.add_argument('--nepoch', type=int, help="which epochs to generate image", default=25)
+parser.add_argument('--nz', type=int, help='size of the latent z vector', default=20)
+parser.add_argument('--vae', action="store_true", help="choose vae model")
+parser.add_argument('-i', '--input-nums', type=int, nargs="*", help="if this argument is specified, the model trained by this number(s) will be used.")
+parser.add_argument('-v', '--valid-nums', type=int, nargs="*", help="which classes to use in determining threshold. if not specified, this will be the same as input-nums")
+parser.add_argument('--image-num', type=int, help="how many images to compare.", default=64)
+args = parser.parse_args()
+
+
+if args.valid_nums is None:
+    valid_nums = args.input_nums
+else:
+    valid_nums = args.valid_nums
+
+valset = SingleMNIST(valid_nums, False)
+valloader = DataLoader(valset, batch_size=1, shuffle=False, num_workers=2)
+
+if args.vae:
+    net = VAE(args.nz)
+else:
+    net = AE(args.nz)
+net.to(device)
+net.eval()
+
+net.load_state_dict(torch.load(net_path(args.nepoch - 1, args.nz, args.vae, args.input_nums)))
+
+criterion = nn.MSELoss()
+
+print('determining threshold...')
+
+losses = []
+for images, label in tqdm(valloader):
+    images = images.to(device)
+    if args.vae:
+        kl, reconst = net.loss(images)
+        loss = kl + reconst
+    else:
+        output = net(images)
+        loss = criterion(output, images)
+    losses.append(loss.item())
+    # print(loss.item())
+
+losses.sort()
+threshold = losses[int(len(losses) * 0.9)]
+
+print(f"threshold = {threshold}")
+
+testset = MNIST('.', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
+
+true_false_positive_case_num = [0] * 10
+num_num = [0] * 10
+
+
+print('calculating false/true positive rate...')
+for image, label in tqdm(testloader):
+    image = image.to(device)
+    label = label.item()
+    if args.vae:
+        kl, reconst = net.loss(image)
+        loss = kl + reconst
+    else:
+        output = net(image)
+        loss = criterion(output, image).item()
+
+    # print(loss, label)
+    if loss > threshold:
+        true_false_positive_case_num[label] += 1
+    num_num[label] += 1
+
+true_false_positive_rates = np.array(true_false_positive_case_num)/np.array(num_num)
+print(true_false_positive_rates)
+
+
+
+
+
+
+    
+
+
+
+
+
--- compare_images.py
+++ compare_images.py
@@ -19,6 +19,7 @@ parser.add_argument('--nz', type=int, help='size of the latent z vector', defaul
 parser.add_argument('--vae', action="store_true", help="choose vae model")
 parser.add_argument('-i', '--input-nums', type=int, nargs="*", help="if this argument is specified, the model trained by this number(s) will be used.")
 parser.add_argument('-t', '--test-nums', type=int, nargs="*", help="which number(s) to use in test.")
+parser.add_argument('--image-num', type=int, help="how many images to compare.", default=64)
 args = parser.parse_args()
 
 if args.test_nums is None:
@@ -26,7 +27,7 @@ if args.test_nums is None:
 else:
     testset = SingleMNIST(args.test_nums, False)
 
-testloader = DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
+testloader = DataLoader(testset, batch_size=args.image_num, shuffle=False, num_workers=2)
 
 if args.vae:
     net = VAE(args.nz)
@@ -46,8 +47,8 @@ def image_path(epoch, nz, vae, real, input_numbers, test_numbers):
     base_dir = mkdir_if_not_exists(f"images/{'vae' if vae else 'ae'}/{input_name}_{test_name}")
     return os.path.join(base_dir, f"z{nz:03d}_e{epoch+1:04d}_{'real' if real else 'fake'}.png")
 
-save_image(images.view(-1, 1, 28, 28), image_path(args.nepoch - 1, args.nz, args.vae, True, args.input_nums, args.test_nums))
+save_image(images.view(-1, 1, 28, 28), image_path(args.nepoch - 1, args.nz, args.vae, True, args.input_nums, args.test_nums), pad_value=1)
 reconst = net(images)
-save_image(reconst.view(-1, 1, 28, 28), image_path(args.nepoch - 1, args.nz, args.vae, False, args.input_nums, args.test_nums))
+save_image(reconst.view(-1, 1, 28, 28), image_path(args.nepoch - 1, args.nz, args.vae, False, args.input_nums, args.test_nums), pad_value=1)
 
 
