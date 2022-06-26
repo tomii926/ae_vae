20577a0f942c89af022b32bb5d09571019e8dab7
Eisuke15 <hideyu41691@gmail.com>
2022-06-26 17:13:13 +0900

    generate image

---
+++ generate_image.py
@@ -0,0 +1,26 @@
+import os
+from argparse import ArgumentParser
+
+import torch
+from torchvision.utils import save_image
+
+from common import device, mkdir_if_not_exists, net_path
+from net import VAE
+
+device = device()
+
+parser = ArgumentParser(description='Generate images from random latent vectors using the learned model.')
+parser.add_argument('--nepoch', type=int, help="number of epochs to generate images", default=50)
+parser.add_argument('--nz', type=int, help='size of the latent z vector', default=16)
+args = parser.parse_args()
+
+net = VAE(args.nz)
+net.to(device)
+net.eval()
+
+fixed_z = torch.randn(64, args.nz).to(device)
+
+print(f'generating epoch={args.nepoch} image')
+net.load_state_dict(torch.load(net_path(args.nepoch-1, args.nz, True)))
+y = net._decoder(fixed_z)
+save_image(y, f'images/z{args.nz:02d}_e{args.nepoch:04d}_.png', pad_value=1)
