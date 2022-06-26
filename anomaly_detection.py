2baebaeb10df1bc75f658c614cc6f81fb9b584f7
Eisuke15 <hideyu41691@gmail.com>
2022-06-26 18:39:23 +0900

    refactoring

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -14,19 +14,20 @@ from common import device, mkdir_if_not_exists, net_path
 from dataset import SingleMNIST
 from net import AE, VAE
 
-device = device()
-
 parser = ArgumentParser(description="Anomaly detection when trained with partial MNIST classes.")
+parser.add_argument('inputnums', type=int, nargs="+", help="The model trained by this classes will be used.")
 parser.add_argument('--nepoch', type=int, help="Which epochs to use for anomaly detection", default=50)
 parser.add_argument('--nz', type=int, help='size of the latent z vector', default=16)
 parser.add_argument('--vae', action="store_true", help="choose vae model")
-parser.add_argument('-i', '--input-nums', type=int, nargs="*", help="The model trained by this classes will be used.")
 parser.add_argument('--no-kl', action="store_true", help="KL divergence is not used in determining the threshold.")
 parser.add_argument('--kl', action="store_true", help="Only KL divergence is used when determining threshold.")
 parser.add_argument('-t', '--threshold', type=float, help="threshold", default=0.99)
+parser.add_argument('-g', '--gpu-num', type=int, help='what gpu to use', default=0)
 args = parser.parse_args()
 
-valset = SingleMNIST(args.input_nums, train=True)
+device = device(args.gpu_num)
+
+valset = SingleMNIST(args.inputnums, train=True)
 valloader = DataLoader(valset, batch_size=64, shuffle=False, num_workers=2)
 
 if args.vae:
@@ -36,7 +37,7 @@ else:
 net.to(device)
 net.eval()
 
-net.load_state_dict(torch.load(net_path(args.nepoch - 1, args.nz, args.vae, args.input_nums)))
+net.load_state_dict(torch.load(net_path(args.nepoch - 1, args.nz, args.vae, args.inputnums)))
 
 criterion = nn.MSELoss(reduction='none')
 
@@ -64,7 +65,7 @@ testloader = DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
 positive_num = [0] * 10
 num_num = [0] * 10
 
-print('calculating false/true positive rate...')
+print('calculating positive rate of MNIST classes')
 for images, labels in tqdm(testloader):
     images = images.to(device)
     labels = labels.tolist()
@@ -82,6 +83,7 @@ for images, labels in tqdm(testloader):
             positive_num[label] += 1
         num_num[label] += 1
 
+print('calculating positive rate of Fashion-MNIST')
 fashionset = FashionMNIST('.', train=False, download=True, transform=ToTensor())
 fashionloader = DataLoader(fashionset, batch_size=64, shuffle=False, num_workers=2)
 
@@ -95,19 +97,16 @@ for images, _ in tqdm(fashionloader):
         output = net(images)
         losses = torch.sum(criterion(output, images), dim=(1, 2, 3))
 
-    losses = losses.tolist()
-
-    for loss in losses:
-        if loss > threshold:
-            positive += 1
+    positive += torch.count_nonzero(losses > threshold).item()
 
 positive_rates = np.array(positive_num)/np.array(num_num)
 positive_rates = np.append(positive_rates, positive/len(fashionset))
+
 print(positive_rates)
 
 left = np.arange(0, 11)
 label = [str(i) for i in range(10)] + ["Fashion"]
 plt.bar(left, positive_rates, tick_label=label)
-input_name = '-'.join(str(n) for n in sorted(args.input_nums)) if args.input_nums else ''
+input_name = '-'.join(str(n) for n in sorted(args.inputnums)) if args.inputnums else ''
 path = os.path.join(mkdir_if_not_exists(f'tables/{"v" if args.vae else ""}ae'), f"{'onlykl' if args.kl else ''}{input_name}_t{args.threshold:.3f}.png")
 plt.savefig(path)
